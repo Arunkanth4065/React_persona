7cece9c541c8100fdec9e5cf9e30637c6eaa908f
Arunkanth4065 <dorepalliarun@gmail.com>
2025-12-21 11:38:52 +0530

    commit chnges

--- Destructuring.js
+++ Destructuring.js
@@ -3,4 +3,9 @@ let cars = ['A','B','C','D'];
 //let car1 = cars[0];
 
 let [car1,car2,car3,car4] = cars;
-console.log(cars);
+console.log(car1,car2,car3);
+
+let numbers = {x:1,y:21,z:31};
+
+let {x,y,z} = numbers;
+console.log(x,y,z);                                                                          
---
+++ map.js
@@ -0,0 +1,5 @@
+let bikes =  ['Hero','Honda','Suzuki','Royal Enfield','BMW'];
+
+const list = bikes.map((bike)=>{
+console.log(bike);
+});
